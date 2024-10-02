1b7989ff4901e9fe0bf53f0f3c164a356f5ac4da
Michele Abategiovanni <michele.abategiovanni@gmail.com>
2024-10-02 14:48:50 +0200

    test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_github_actions/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke tests S', (WidgetTester tester) async {
+  testWidgets('Counter increments smoke tests', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
